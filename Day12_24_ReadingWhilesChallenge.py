f2b66f66f2488993a1a4f749ccf15de173c6ba07
TurtleWolfe <7327259+TurtleWolfe@users.noreply.github.com>
2022-01-26 15:58:46 +0000

    harness 25

--- Day12_24_ReadingWhilesChallenge.py
+++ Day12_24_ReadingWhilesChallenge.py
@@ -5,7 +5,7 @@
 # Day 12
 # While Loops (2 hours)
 #################################################
-
+def numList(i):
 # Given the following input entered by the user, what will be output?
 # Henry Higgins
 # Splendid. And you?
@@ -15,6 +15,23 @@
 # I especially like The Ministry of Silly Walks and Blackmail skits
 # Wonderful, but I may have to quit soon. It takes too much of my time.
 # Goodbye
+  numbers=[]
+  while i != 0:
+    i = float(input("enter a number, or '0' when done: "))
+    numbers.append(i)
+    # numbers = numbers.extend(a)
+    # numbers = numbers.insert(0,a)
+    # numbers = numbers.sort()
+    print()
+    print()
+    print("Sort: ", numbers[:-1])
+    print("Sum: ", numbers[:-1])
+    print("Average: ", numbers[:-1])
+    # print(i)
+    # print(numbers[:-1])
+  # return(i)
+  return(numbers)
+# return(numList)
 
 #################################################
 
@@ -24,10 +41,19 @@ def main():
   print("Day12_24_ReadingWhilesChallenge")
   print()
 
-  # # Testing Template
-  # test_input = ["hellothere", "42 degrees Celsius", "eeeeeee"]
-  # test_output = ["HELOTHRE", "42DEGREES CELSUS", "EEEEE"]
-  # for i in range(len(test_input)):
-  #   print("Testing: ", test_input[i] +":", mangle(test_input[i]) == test_output[i])
-    
-main()
+# why this one print zero?
+  # print(fourLoopz(4)) 
+  # print()
+
+  # print(csciOneFifty("csciOneFifty"))  
+  # print(csciOneFifty("CSCI 150"))
+
+  # print()
+  # numList("9")
+  numList(9)
+  # numList("0")
+  # numList(0)
+  # numList()
+  # print(numList("0"))  
+  print()
+main()  # end of Curled Main.. 
--- Day12_25_ReadingWhilesEnrichment.py
+++ Day12_25_ReadingWhilesEnrichment.py
@@ -5,7 +5,10 @@
 # Day 12
 # While Loops (2 hours)
 #################################################
-
+def numList(i):
+  return(i)
+  # return(numbers)
+# return(numList)
 # John Lennon is my favorite beatle
 # Paul McCartney is my favorite beatle
 # George Harrison is my favorite beatle
@@ -23,10 +26,12 @@ def main():
   print("Day 12 While Loops")
   print()
 
-  # # Testing Template
-  # test_input = ["hellothere", "42 degrees Celsius", "eeeeeee"]
-  # test_output = ["HELOTHRE", "42DEGREES CELSUS", "EEEEE"]
-  # for i in range(len(test_input)):
-  #   print("Testing: ", test_input[i] +":", mangle(test_input[i]) == test_output[i])
-    
-main()
+  # print()
+  # numList("9")
+  # numList(9)
+  # numList("0")
+  # numList(0)
+  # numList()
+  print(numList("The Beatles"))  
+  print()
+main()  # end of Curled Main.. 
