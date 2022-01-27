e9406393eea94b2f3496f885d672fc3e72c9ba11
TurtleWolfe <7327259+TurtleWolfe@users.noreply.github.com>
2022-01-27 19:41:03 +0000

    harness 12_24

--- Day12_23_WritingWhiles.py
+++ Day12_23_WritingWhiles.py
@@ -1,7 +1,7 @@
 # Joy of Coding
 # via Prof. Emily Hill
 # Beginning Python Challenge
-
+            # by, TurtleWolfe.com
 # Day 12
 # Practice: Writing While Loops (2 hours)
 # print the following using while loops:
@@ -15,29 +15,23 @@ def countTo_i(loopZ):
   while i < loopZ+1:
     print(i)
     i+=1
-  print()
   # print("countTo_i")
   print()
   return()
 # return(countTo_i)
-
 def loop_leapThree(maX_loopZ):  
-
 # 2
 # 5
 # 8
 # 11
-
   i=2
   # print("loop_leapThree")
   # print()
   while i < maX_loopZ+1:
     print(i)
     i+=3
-
   return(i)
 # return(loop_leapThree)
-
 def threeLoopz(i):  
 # -10 -8 -6 -4 -2 0
   while i < 1:
@@ -57,7 +51,7 @@ def fourLoopz(i):
     print("****")
   print()
   return(i)
-# return(fourLoopz)
+# return(fourLoopz) // refactor for 2 more inputs
 
 def csciOneFifty(str):
 # Write a while loop that prints the letters in “CSCI 150” on separate lines.
@@ -66,9 +60,16 @@ def csciOneFifty(str):
   #   print(str[i])
   # print()
   # while i != 0:
-  return(str)
+  # return(str)
+  # Python program to iterate over characters of a string
+  
+  # Code #2
+  # str = "GEEKS"
+    
+  # Iterate over index
+  for element in range(0, len(str)):
+      print(str[element])
 # return(csciOneFifty)
-
 def numList(i):
 # Create a program that allows the user to enter in a list of numbers, prints them out in sorted order, and prints the sum and average of the numbers. Prompt the user to continue entering numbers, and enter the number ‘0’ when finished.
   numbers=[]
@@ -78,39 +79,41 @@ def numList(i):
     # numbers = numbers.extend(a)
     # numbers = numbers.insert(0,a)
     # numbers = numbers.sort()
+# https://www.geeksforgeeks.org/find-average-list-python/?ref=lbp
     print()
     print()
-    print("Sort: ", numbers[:-1])
-    print("Sum: ", numbers[:-1])
-    print("Average: ", numbers[:-1])
+    srtd = sorted(numbers[:-1])
+    print("Sort:    ", srtd)
+    tot = sum(numbers[:-1])
+    print("Sum:     ", tot)
+    leng = len(numbers)-1
+    print("length:  ", leng)
+    if leng and tot > 0:
+      averAge = (tot/leng)
+      # print("Average: ", numbers[:-1])
+      print("Average: ", averAge)
     # print(i)
     # print(numbers[:-1])
   # return(i)
   return(numbers)
 # return(numList)
-
 def main():
   print("Day12_23_WritingWhiles")
   print()
-  countTo_i(5) 
-  # countTo_i(9) 
+  countTo_i(5)#       count to i
+  loop_leapThree(11)# by threes to i
+  print()
+  threeLoopz(-10)#    -10 -8 -6 -4 -2 0 
+  print()
+  fourLoopz(4)#       ****
   # print()
-
-  loop_leapThree(11)
+  # csciOneFifty("csciOneFifty")  
+  # print("-"*20)
+  csciOneFifty("CSCI 150")
   print()
-
-  threeLoopz(-10) 
-  print()
-
-  fourLoopz(4) 
-  print()
-
-  print(csciOneFifty("csciOneFifty"))  
-  print(csciOneFifty("CSCI 150"))
-  # print()
   numList("9")
   # numList(9)
   # numList("0")
   # numList(0)
   print()
-main()  # end of Curled Main.. 
+# main()  # end of Curled Main.. 
